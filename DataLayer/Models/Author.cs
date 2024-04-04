70f81082d13c3c833a600e4b8f7540f262de9764
CatalinRapeanu <129482215+CatalinRapeanu@users.noreply.github.com>
2024-04-04 15:45:57 +0300

    Delete DataLayer directory

--- DataLayer/Models/Author.cs
+++
@@ -1,9 +0,0 @@
-namespace DataLayer.Models;
-
-public class Author
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; }
-     
-    public List<Book> Books { get; set; }
-}
--- DataLayer/Models/MovieDirector.cs
+++
@@ -1,8 +0,0 @@
-namespace DataLayer.Models;
-
-public class MovieDirector
-{
-    public Guid MovieId { get; set; }
-    public Guid DirectorId { get; set; }
-    
-}
